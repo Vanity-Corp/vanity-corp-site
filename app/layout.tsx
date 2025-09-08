49d4ee2ab70f0b6244e7bee7d7cc713d91fcec7f
MohMost <teemohmost2020@gmail.com>
2025-09-08 15:46:38 +0100

    ✨ (feat) : add the services mega menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,9 @@ import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
-import { HeroHighlight } from "@/components/ui/hero-highlight";
-
 import { Footer } from "@/components/Footer";
-import BannerSlider from "@/components/BannerSlider";
-import Head from "next/head";
-import Script from "next/script";
 import GoogleAnalytics from "@/components/GoogleAnalytics";
+
 const montserrat = Montserrat({ subsets: ["latin"], weight: ["600"] });
 
 export const metadata: Metadata = {
@@ -20,14 +16,16 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en" className="dark">
       <GoogleAnalytics GA_MEASUREMENT_ID={`${process.env.GOOGLE_ANALYTICS}`} />
 
       <body className={montserrat.className}>
+        {/* Provide modal context globally */}
+
         <Navbar />
         <div className="flex flex-row">
           <Sidebar />
